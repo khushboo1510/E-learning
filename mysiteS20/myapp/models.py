3f733c25070042d0df2a939bbb3d33fbf0be5068
khushboo1510 <khushboomehta1995@gmail.com>
2020-06-23 18:20:40 -0400

    Added Student, Course and Topic models

--- mysiteS20/myapp/models.py
+++ mysiteS20/myapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db import models
+import datetime
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Course(models.Model):
+    topic = models.ForeignKey(Topic, related_name='courses', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    for_everyone = models.BooleanField(default=True)
+    description = models.TextField(max_length=300, null=True, blank=True)
+
+
+class Student(User):
+    CITY_CHOICES = [('WS', 'Windsor'),
+    ('CG', 'Calgery'),
+    ('MR', 'Montreal'),
+    ('VC', 'Vancouver')]
+    school = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=2, choices=CITY_CHOICES, default='WS')
+    interested_in = models.ManyToManyField(Topic)
