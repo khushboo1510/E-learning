98df1b4a67e75b79773022c35a53789135284034
khushboo1510 <khushboomehta1995@gmail.com>
2020-06-23 19:45:21 -0400

    implemented __str__ methods for all models

--- mysiteS20/myapp/models.py
+++ mysiteS20/myapp/models.py
@@ -11,6 +11,10 @@ class Topic(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200, null=True)
 
+    def __str__(self):
+        return '{ Name: ' + self.name + \
+               '- Category: ' + self.category + ' }'
+
 
 class Course(models.Model):
     topic = models.ForeignKey(Topic, related_name='courses', on_delete=models.CASCADE)
@@ -18,6 +22,13 @@ class Course(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     for_everyone = models.BooleanField(default=True)
     description = models.TextField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return '{ Name: ' + self.name + \
+               '- Price: ' + str(self.price) + \
+               '- For Everyone: ' + str(self.for_everyone) + \
+               '- Topic: ' + self.topic.name + \
+               '- Description: ' + (self.description or 'NA') + '} '
 
 
 class Student(User):
@@ -29,6 +40,12 @@ class Student(User):
     city = models.CharField(max_length=2, choices=CITY_CHOICES, default='WS')
     interested_in = models.ManyToManyField(Topic)
 
+    def __str__(self):
+        return '{ Name: ' + self.first_name + ' ' + self.last_name + \
+               '- School: ' + self.school + \
+               '- City: ' + self.city + \
+               '- Interested in: ' + str(self.interested_in.values_list('name')) + '} '
+
 
 class Order(models.Model):
     ORDER_STATUS_CHOICES = [(0, 'Cancelled'), (1, 'Order Confirmed')]
@@ -38,7 +55,14 @@ class Order(models.Model):
     order_status = models.IntegerField(choices=ORDER_STATUS_CHOICES, default=1)
     order_date = models.DateField(auto_now_add=True, editable=False)
 
-
-
+    def total_cost(self):
+        return sum(course.price for course in self.course.all())
+    
+    def __str__(self):
+        return '{ Student: ' + str(self.student.first_name + ' ' + self.student.last_name) + \
+               '- Courses: ' + str(self.course.values_list('name')) + \
+               '- Levels: ' + str(self.levels) + \
+               '- Order Status: ' + str(self.order_status) + \
+               '- Order Date: ' + str(self.order_date) + '}'
 
 
