45c16160a95d0d940d4d53df2494cafb5dbc1289
Aditi Desai <55603078+aditi795@users.noreply.github.com>
2020-06-30 15:49:12 -0400

    views.py and urls.py created and updated

--- mysiteS20/myapp/views.py
+++ mysiteS20/myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from .models import Topic, Course, Student, Order
+
+
 # Create your views here.
+def index(request):
+    top_list = Topic.objects.all().order_by('id')[:10]
+    response = HttpResponse()
+    heading1 = '<p>' + 'List of topics: ' + '</p>'
+    response.write(heading1)
+    for topic in top_list:
+        para = '<p>' + str(topic.id) + ': ' + str(topic) + '</p>'
+        response.write(para)
+    return response
