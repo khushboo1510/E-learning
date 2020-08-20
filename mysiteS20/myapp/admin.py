421f7c056b419109c5de44668d0a93e6808dea61
khushboo1510 <khushboomehta1995@gmail.com>
2020-08-19 23:33:16 -0400

    add action to add 10 hours to the course in admin site

--- mysiteS20/myapp/admin.py
+++ mysiteS20/myapp/admin.py
@@ -1,3 +1,4 @@
+import decimal
 
 from django.contrib import admin
 from .models import Topic, Course, Student, Order
@@ -9,8 +10,18 @@ admin.site.register(Order)
 admin.site.register(Student)
 
 
+def add_50_to_hours(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.hours = obj.hours + decimal.Decimal('10.0')
+        obj.save()
+
+
+add_50_to_hours.short_description = "Add 10 hours to selected Courses"
+
+
 class CourseAdmin(admin.ModelAdmin):
     list_display = ["name", "topic", "price", "hours", "for_everyone"]
+    actions = [add_50_to_hours]
 
     class Meta:
         model = Course
